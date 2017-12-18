28f1b089b24861687b5fd492a4e21f76eec8befc
abshriva <shaky2108@gmail.com>
2017-12-18 13:15:17 +0530

    commit

---
+++ src/app/employee-detail/employee-detail.component.ts
@@ -0,0 +1,37 @@
+
+import { Component, OnInit,Input } from '@angular/core';
+import { Employee } from '../models/Employee';
+
+/**
+ * 
+ * 
+ * @export
+ * @class EmployeeDetailComponent
+ * @implements {OnInit}
+ */
+@Component({
+  selector: 'app-employee-detail',
+  templateUrl: './employee-detail.component.html',
+  styleUrls: ['./employee-detail.component.css']
+  
+})
+
+export class EmployeeDetailComponent implements OnInit {
+
+  /**
+   * 
+   * 
+   * @memberof EmployeeDetailComponent
+   */
+  @Input() empdetail;
+  imageUrl = 'https://s3.amazonaws.com/uifaces/faces/twitter/fffabs/128.jpg';
+  employeeList: Employee;
+  
+  
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
---
+++ src/app/services/employee-service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Employee } from '../models/Employee';
+//import { EmployeeType } from '../models/Employee-type';
+
+/**
+ * 
+ * 
+ * @export
+ * @class EmployeeService
+ */
+@Injectable()
+
+
+export class EmployeeService{
+
+    emp:Employee[] =[];
+   
+
+      getEmployees(): Employee[]
+      { 
+      return this.emp;
+      }
+        
+      createEmployee(newEmp: Employee)
+      {
+      this.emp.unshift(newEmp);
+      
+      }
+        
+}
